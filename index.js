768bcdb715f3315e33f96dedb651eaa83da6a700
DYoshizawa079 <daniely079@gmail.com>
2021-12-24 18:53:30 -0500

    Got async and await kinda figured out

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ const fs = require('fs');
 const generateTeam = require('./src/template.js');
 const inquirer = require('inquirer');
 
-
 // New hire modual
 const Intern = require('./library/Intern.js');
 const Engineer = require('./library/Engineer.js');
 const Manager = require('./library/Manager.js');
 
-
 //Answers to Questions array
 const newStaffData = [];
 
 //Array of questions asked to users
-const questions = () => {
-  const answer =   
-   inquirer.prompt([
+const questions = async () => {
+  const answer = await inquirer.prompt([
         {
             type: "input",
             name: "name",
@@ -37,74 +34,78 @@ const questions = () => {
             message: "Please select your role.",
             choices: ["Intern", "Engineer", "Manager"],
         },
-    ]).then(answer => {
-        console.log(answer);
-    })
+    ])
+    console.log(answer);
+    positionQs (answer);
 }
-    questions()
 
+// Ask questions based on the employee's position
+const positionQs = async (answer) => {
 
-// If intern has been selected than proceed to answer these questions
-if (answer.role === "Engineer") {
-    const githubAnswer = inquirer
-    .prompt([
-        {
-        type: "input",
-        name: "github",
-        message: "Enter your Github user name.",
-        },  
-     ])
-     
-    const newEngineer = new Engineer(
-        answer.name,
-        githubAnswer.github,
-        answer.id,
-        answer.email,
-    );
-    newNewHireMemberData.push(newEngineer);
+    // If employee is an Engineer
+    if (answer.role === "Engineer") {
+        const githubAnswer = await inquirer.prompt([
+            {
+            type: "input",
+            name: "github",
+            message: "Enter your Github user name.",
+            },  
+        ])
+        console.log(githubAnswer);
+         
+        /* const newEngineer = new Engineer(
+            answer.email,
+            answer.name,
+            answer.id,
+            answer.role,
+            githubAnswer,
+        );
+        console.log(newEngineer); */
+        //newNewHireMemberData.push(newEngineer); 
 
-// If manager has been selected than proceed to answer these questions
-}else if (answer.role === "Manager") {
-    const managerAnswer = 
-     inquirer.prompt([
-        {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your office number?",
-        }, 
-    ])
+    // If employee is a Manager
+    } else if (answer.role === "Manager") {
+        const managerAnswer = await inquirer.prompt([
+            {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?",
+            }, 
+        ])
+        console.log(managerAnswer);
+        
+        /* const newManager = new Manager(
+            answer.name,
+            managerAnswer.officeNumber,
+            answer.id,
+            answer.email,
+        );
+        console.log(newManager); */
+        //newNewHireMemberData.push(newManager);
+
+    // If employee is an intern
+    } else if (answer.role === "Intern") {
+        const internAnswer = await inquirer.prompt([
+            {
+            type: "input",
+            name: "school",
+            message: "Enter in the name of the university you attented.",
+            },  
+        ])
+        console.log(internAnswer)
     
-    const newManager = new Manager(
-        answer.name,
-        managerAnswer.officeNumber,
-        answer.id,
-        answer.email,
-    );
-    newNewHireMemberData.push(newManager);
-// If intern has been selected than proceed to answer these questions.
+        /* const newIntern = new Intern(
+            answer.name,
+            internAnswer.school,
+            answer.id,
+            answer.email,
+        );
+        console.log(newIntern); */
+        //newNewHireMemberData.push(newIntern);
+    }
+}
 
-}else if (answer.role === "Intern") {
-    const internAnswer = 
-     inquirer.prompt([
-        {
-        type: "input",
-        name: "school",
-        message: "Enter in the name of the university you attented.",
-        },  
-    ])
-
-    const newIntern = new Intern(
-        answer.name,
-        internAnswer.school,
-        answer.id,
-        answer.email,
-    );
-    newNewHireMemberData.push(newIntern)
-    ;
-    
-    }; //End of question
-    
-    const addNewMemberAnswer = inquirer
+/*const addNewMemberAnswer = inquirer
     .prompt([
         {
             type: "list",
@@ -118,10 +119,12 @@ if (answer.role === "Engineer") {
     
     }
     // return createTeam ();
-
+*/
     
-    const createTeam = () => {
-        console.log ("new member", newStaffMemberData)
-        fs.writeFileSync("./front-end/index.html", generareTeam(newstaffMemberData),"utf-8"
-        );
-    }
+/* const createTeam = () => {
+    console.log ("new member", newStaffMemberData)
+    fs.writeFileSync("./front-end/index.html", generareTeam(newstaffMemberData),"utf-8"
+    );
+}  */
+
+questions()
